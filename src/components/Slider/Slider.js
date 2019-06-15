96083a644bd43ea76ada0aa4760b0fdd4af7990d
Nerijus <nerius10@gmail.com>
2019-06-15 18:20:59 +0300

    Adding spinner and error messages

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 import { Card } from 'react-bootstrap';
 import Slider from 'react-slick';
+import Spinner from '../UI/Spinner/Spinner';
 
 class Heading extends Component {
   render() {
@@ -45,6 +46,49 @@ class Heading extends Component {
 
     let recipes = this.props.latestRecipes.slice(0, 5);
 
+    let slider = this.props.loading ? (
+      <Spinner />
+    ) : (
+      <Slider {...settings}>
+        {recipes.map(recipe => {
+          return (
+            <div key={recipe.idMeal}>
+              <Card>
+                <Card.Img variant='top' src={recipe.strMealThumb} />
+                <Card.Body>
+                  <Card.Title>
+                    <i className='fas fa-star full' />
+                    <i className='fas fa-star full' />
+                    <i className='fas fa-star full' />
+                    <i className='fas fa-star empty' />
+                    <i className='fas fa-star empty' />
+                    <br />
+                    <h2>{recipe.strMeal}</h2>
+
+                    <hr />
+                  </Card.Title>
+                  <Card.Text>
+                    #{recipe.strArea} #{recipe.strCategory}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+          );
+        })}
+      </Slider>
+    );
+
+    const heading =
+      this.props.errorMsg.length > 0 ? (
+        <h1 className='text-centered'>Sorry! Something went wrong :(</h1>
+      ) : (
+        <React.Fragment>
+          <h1 className='text-centered'>Welcome to our recipe blog</h1>
+          <h5 className='text-centered'>
+            Over 5000 recipes <br /> The most delicious experience for You
+          </h5>
+        </React.Fragment>
+      );
     return (
       <div
         className='slider-wrap'
@@ -52,38 +96,8 @@ class Heading extends Component {
           backgroundSize: 'cover'
         }}
       >
-        <h1 className='text-centered'>Welcome to our recipe blog</h1>
-        <h5 className='text-centered'>
-          Over 5000 recipes <br /> The most delicious experience for You
-        </h5>
-
-        <Slider {...settings}>
-          {recipes.map(recipe => {
-            return (
-              <div key={recipe.idMeal}>
-                <Card>
-                  <Card.Img variant='top' src={recipe.strMealThumb} />
-                  <Card.Body>
-                    <Card.Title>
-                      <i className='fas fa-star full' />
-                      <i className='fas fa-star full' />
-                      <i className='fas fa-star full' />
-                      <i className='fas fa-star empty' />
-                      <i className='fas fa-star empty' />
-                      <br />
-                      <h2>{recipe.strMeal}</h2>
-
-                      <hr />
-                    </Card.Title>
-                    <Card.Text>
-                      #{recipe.strArea} #{recipe.strCategory}
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </div>
-            );
-          })}
-        </Slider>
+        {heading}
+        {slider}
       </div>
     );
   }
@@ -91,7 +105,9 @@ class Heading extends Component {
 
 const mapStateToProps = state => {
   return {
-    latestRecipes: state.latest.latestRecipes
+    latestRecipes: state.latest.latestRecipes,
+    loading: state.latest.loading,
+    errorMsg: state.latest.errorMsg
   };
 };
 
--- src/containers/Categories/Categories.js
+++ src/containers/Categories/Categories.js
@@ -5,34 +5,52 @@ import Heading from '../../components/Heading/Heading';
 import Jumbotron from '../../components/UI/Jumbotron/Jumbotron';
 import Footer from '../../components/Footer/Footer';
 import { Card } from 'react-bootstrap';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Categories extends Component {
   componentDidMount() {
     this.props.fetchCategories();
   }
   render() {
+    const wrapperStyle = {
+      display: this.props.loading
+        ? 'block'
+        : this.props.errorMsg.length > 0
+        ? 'block'
+        : 'grid'
+    };
+    const content = this.props.loading ? (
+      <Spinner />
+    ) : (
+      this.props.categories.map(cat => {
+        let excerpt = cat.strCategoryDescription.split('.').slice(0, 1);
+        console.log(excerpt);
+        return (
+          <Card>
+            <Card.Img variant='top' src={cat.strCategoryThumb} />
+            <Card.Body>
+              <Card.Title>
+                <br />
+                <h2>{cat.strCategory}</h2>
+                <hr />
+              </Card.Title>
+              <Card.Text>{excerpt}</Card.Text>
+            </Card.Body>
+          </Card>
+        );
+      })
+    );
+    const error =
+      this.props.errorMsg.length > 0 ? (
+        <h1 style={{ textAlign: 'center' }}>Sorry! Something went wrong :(</h1>
+      ) : null;
     return (
       <div>
         <Heading />
         <Jumbotron location='Categories' />
-        <div className='categories-wrapper'>
-          {this.props.categories.map(cat => {
-            let excerpt = cat.strCategoryDescription.split('.').slice(0, 1);
-            console.log(excerpt);
-            return (
-              <Card>
-                <Card.Img variant='top' src={cat.strCategoryThumb} />
-                <Card.Body>
-                  <Card.Title>
-                    <br />
-                    <h2>{cat.strCategory}</h2>
-                    <hr />
-                  </Card.Title>
-                  <Card.Text>{excerpt}</Card.Text>
-                </Card.Body>
-              </Card>
-            );
-          })}
+        <div className='categories-wrapper' style={wrapperStyle}>
+          {error}
+          {content}
         </div>
         <Footer />
       </div>
@@ -42,7 +60,9 @@ class Categories extends Component {
 
 const mapStateToProps = state => {
   return {
-    categories: state.categories.categories
+    categories: state.categories.categories,
+    loading: state.categories.loading,
+    errorMsg: state.categories.errorMsg
   };
 };
 
