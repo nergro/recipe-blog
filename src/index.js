5644e78b8f334c8797b39c0ddc3fb668ddc417ed
Nerijus <nerius10@gmail.com>
2019-07-31 22:00:24 +0300

    Removing redux devtools setup

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import thunk from "redux-thunk";
 
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import latestReducer from './store/reducers/latest';
-import categoriesReducer from './store/reducers/categories';
-import categoryReducer from './store/reducers/category';
-import recipeReducer from './store/reducers/recipe';
-import flagsReducer from './store/reducers/flags';
-import autocomplete from './store/reducers/autocomplete';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import latestReducer from "./store/reducers/latest";
+import categoriesReducer from "./store/reducers/categories";
+import categoryReducer from "./store/reducers/category";
+import recipeReducer from "./store/reducers/recipe";
+import flagsReducer from "./store/reducers/flags";
+import autocomplete from "./store/reducers/autocomplete";
 
-const composeEnhancers =
-  process.env.NODE_ENV === 'development'
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : null || compose;
+// const composeEnhancers =
+//   process.env.NODE_ENV === 'development'
+//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+//     : null || compose;
 
 const rootReducer = combineReducers({
   latest: latestReducer,
@@ -31,10 +31,7 @@ const rootReducer = combineReducers({
   search: autocomplete
 });
 
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
-);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const app = (
   <Provider store={store}>
@@ -44,6 +41,6 @@ const app = (
   </Provider>
 );
 
-ReactDOM.render(app, document.getElementById('root'));
+ReactDOM.render(app, document.getElementById("root"));
 
 serviceWorker.unregister();
